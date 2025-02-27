f54cf43d63111e3aab9264b3b76aa313550088d1
KenjiKawabata <kawabatatbegoat@gmail.com>
2025-02-27 18:38:26 +0900

    lesson done

--- sql-models-migrations/airline/flights/models.py
+++ sql-models-migrations/airline/flights/models.py
@@ -13,7 +13,7 @@ class Flight(models.Model):
     #origin = models.CharField(max_length=64)
     #destination = models.CharField(max_length=64)
     # on_delete=models.CASCADE ここではAirportを削除した場合、それに対応するFlightも削除される
-    # related_nameはdjangoが呼ぶときのエイリアスみたいな感じ？
+    # related_nameは逆参照を可能にする。AirportからFlightのoriginをdepartures、destinationをarrivalsとして参照できる。
     origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
     destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
@@ -21,3 +21,13 @@ class Flight(models.Model):
     # この関数がオブジェクトの文字列表現を返す場合
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    # flights属性を使ってそのPassengerに関連する全てのFlightにアクセスできる。
+    # 同様にpassengersというrelated_nameを使ってそのFlightの全てのPassengerにアクセスできる。
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
--- sql-models-migrations/airline/flights/views.py
+++ sql-models-migrations/airline/flights/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
-from .models import Flight
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+def flight(request, flight_id):
+    # flight_idを主キーとするFlightを取得
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+         # Passengerクラスのrelated_nameでpassengersを指定しているのでPassengerクラスのflightsを逆参照をしている
+        "passengers": flight.passengers.all(),
+        # このFlightに乗るPassengerを除外
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
+    })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        # flight_idを主キーとするFlightを取得
+        flight = Flight.objects.get(pk=flight_id)
+        # Passengerについての情報の取得
+        #   主キーがpassengerという名前の入力フォームから渡されるintのPassenger
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        # ↑のpassengerのflightsに最初に取得したFlightを追加する
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
